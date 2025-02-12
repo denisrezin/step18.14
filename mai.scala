6457fe7333413050756e3992b4e281a484699099
denisrezin <denisrezin@gmail.com>
2025-02-12 13:59:40 +0800

    Create main.scala

---
+++ mai.scala
@@ -0,0 +1,24 @@
+// Это исходный код, его можно раскомментировать и посмотреть
+// object StringProcessor {
+//   def processStrings(strings: List[String]): List[String] = {
+//     var result = List[String]()
+//     for (str <- strings) {
+//       if (str.length > 3) {
+//         result = result :+ str.toUpperCase
+//       }
+//     }
+//     result
+//   }
+
+
+// это исправленный код
+object MyStringProcessor extends App {
+  def processStrings(strings: List[String], l: Int = 3): List[String] = {
+    strings.filter(_.length > l).map(_.toUpperCase)
+  }
+
+  val strings = List("apple", "cat", "banana", "dog", "elephant")
+  // val my_processed_Strings = processStrings(strings)
+  val my_processed_Strings = processStrings(strings, 5)
+  println(s"Processed strings: $my_processed_Strings")
+}
